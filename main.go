fe4e302ed4f8c83bc89fe43f8b342682e898ba6c
Victor Sergienko <vik.sergienko@mail.ru>
2021-03-15 19:15:54 +0300

    Add base handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4"
@@ -17,21 +18,43 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello world' ").Scan(&greeting)
+	var id int
+	err = conn.QueryRow(context.Background(), "INSERT INTO urls (id, full_address_name, short_key) VALUES ($1, $2, $3) RETURNING id", "1", "aaa", "eee").Scan(&id)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(greeting)
-
 	route := mux.NewRouter()
 
-	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to this life-changing API.")
-	})
+	route.HandleFunc("/", Index)
+	route.HandleFunc("/{key}", Redirect)
+	route.HandleFunc("/create", CreateShortUrl).Methods("POST")
 
 	fmt.Println("Server listening!")
 	http.ListenAndServe(":5656", route)
+}
+
+func CreateShortUrl(w http.ResponseWriter, r *http.Request) {
+	type UrlDto struct {
+		Url string `json:"url"`
+	}
+	var urlDto UrlDto
+
+	err := json.NewDecoder(r.Body).Decode(&urlDto)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	shortUrl := urlDto.Url
+	json.NewEncoder(w).Encode(map[string]string{"data": shortUrl})
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://github.com/Elvilius", http.StatusMovedPermanently)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to this life-changing API.")
 }
