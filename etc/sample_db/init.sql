a617f3629559b243cfccd473ddf48399b7a9b76d
Victor Sergienko <vik.sergienko@mail.ru>
2021-03-16 18:38:06 +0300

    Fix init sql

--- etc/sample_db/init.sql
+++ etc/sample_db/init.sql
@@ -6,22 +6,7 @@ GRANT ALL PRIVILEGES ON DATABASE short_line TO short_line;
 
 
 CREATE TABLE public.urls (
-    id integer NOT NULL,
+    id  SERIAL PRIMARY KEY,
     full_address_name TEXT,
     short_key TEXT NULL
 );
-
-
-CREATE SEQUENCE public.urls_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.urls_id_seq OWNER TO short_line;
-
-
-ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
